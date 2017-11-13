4d659ede1b189efe7e8b1c08abc8ae066e86d5a5
ylqi007 <yunlongqi@my.unt.edu>
2017-11-12 22:14:22 -0600

    .3, Stack, ResizingArrayStack.java

---
+++ src/BagsQueuesAndStacks/ResizingArrayStack.java
@@ -0,0 +1,96 @@
+package BagsQueuesAndStacks;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class ResizingArrayStack<Item> implements Iterable<Item> {
+
+	private Item[] a;         // array of items
+	private int n;            // number of elements on stack
+	
+	/**
+	 * Initializes an empty stack
+	 */
+	public ResizingArrayStack() {
+        a = (Item[]) new Object[2];
+        n = 0;
+    }
+	
+	public boolean isEmpty() {
+        return n == 0;
+    }
+	
+	public int size() {
+        return n;
+    }
+	
+	public void resize(int capacity) {
+		assert capacity >= n;
+		Item[] temp = (Item[]) new Object[capacity];
+		for(int i=0; i<n; i++) {
+			temp[i] = a[i];
+		}
+		a = temp;
+	}
+	
+	public void push(Item item) {
+        if (n == a.length) resize(2*a.length);    // double size of array if necessary
+        a[n++] = item;                            // add item
+    }
+	
+    public Item pop() {
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        Item item = a[n-1];
+        a[n-1] = null;                              // to avoid loitering
+        n--;
+        // shrink size of array if necessary
+        if (n > 0 && n == a.length/4) resize(a.length/2);
+        return item;
+    }
+    
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        return a[n-1];
+    }
+    
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+    
+    // an iterator, doesn't implement remove() since it's optional
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i;
+
+        public ReverseArrayIterator() {
+            i = n-1;
+        }
+
+        public boolean hasNext() {
+            return i >= 0;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return a[i--];
+        }
+    }
+    
+    public static void main(String[] args) {
+        ResizingArrayStack<String> stack = new ResizingArrayStack<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) stack.push(item);
+            else if (!stack.isEmpty()) StdOut.print(stack.pop() + " ");
+        }
+        StdOut.println("(" + stack.size() + " left on stack)");
+    }
+	
+	
+}
